fb6f476187c52982c65cd14e39ee5ddd49cd735b
edsion1107 <edsion@i1hao.com>
2025-07-07 18:25:31 +0800

    build: Optimized the selectOnlineDevice logic, now it will not be blocked continuously.

--- build-logic/convention/src/main/kotlin/io/appium/multiplatform/AdblibExtensions.kt
+++ build-logic/convention/src/main/kotlin/io/appium/multiplatform/AdblibExtensions.kt
@@ -1,16 +1,45 @@
 package io.appium.multiplatform
 
 import com.android.adblib.*
-import org.gradle.api.GradleException
+import java.time.Duration
+import java.util.concurrent.TimeoutException
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
-suspend fun AdbSession.selectOnlineDevice(serial: String?): ConnectedDevice {
+/**
+ * Selects an online Android device.
+ *
+ * If a [serial] number is provided, it waits for the device with that specific serial number to become available.
+ * If [serial] is null or blank, it searches for the first available online device.
+ *
+ * @param serial The serial number of the target device. If null, the first online device will be selected.
+ * @param timeout The maximum time to wait for the device to be connected.
+ * @return The [ConnectedDevice] instance.
+ * @throws IllegalStateException if no online device is found or if the timeout is reached.
+ */
+suspend fun AdbSession.selectOnlineDevice(
+    serial: String?,
+    timeout: Duration = 10.seconds.toJavaDuration()
+): ConnectedDevice {
     return serial
         ?.takeIf { it.isNotBlank() }
-        ?.let { connectedDevicesTracker.waitForDevice(it) }
+        ?.let {
+            connectedDevicesTracker.waitForDevice(it, timeout)
+        }
         ?: hostServices
             .devices(AdbHostServices.DeviceInfoFormat.BINARY_PROTO_FORMAT)
             .onEach { host.logger.info { "deviceInfo: $it" } }
             .firstOrNull { it.deviceState == DeviceState.ONLINE }
-            ?.let { connectedDevicesTracker.waitForDevice(it.serialNumber) }
-        ?: throw GradleException("No online device found")
+            ?.let { connectedDevicesTracker.waitForDevice(it.serialNumber, timeout) }
+        ?: throw IllegalStateException("No online device found")
+}
+
+suspend fun ConnectedDevicesTracker.waitForDevice(serial: String, timeout: Duration): ConnectedDevice {
+    try {
+        return session.withErrorTimeout(timeout) {
+            waitForDevice(serial)
+        }
+    } catch (e: TimeoutException) {
+        throw IllegalStateException("Could not connect to device: $serial", e)
+    }
 }
